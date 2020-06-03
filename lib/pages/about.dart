8bc49a8a9a0efcbf443323305078a09038fd1fdc
Brieden <Sven.Brieden@posteo.de>
2020-06-03 08:22:06 +0200

    About Crowdy link the homepage now clickable

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'dart:async';
+
+import 'package:url_launcher/url_launcher.dart';
 
 /// This is the stateless widget that the main application instantiates.
 class About extends StatelessWidget {
   static const String route = 'about';
   About({Key key}) : super(key: key);
 
-  Image getLevelIcon(int points){
-
-  }
+  Image getLevelIcon(int points) {}
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +26,23 @@ class About extends StatelessWidget {
               child: Text(''),
             ),
             Flexible(
-              child: Text("Developed by Crowdy. www.crowdy.ch")
+              child: Linkify(
+                onOpen: _onOpen,
+                text: "Developed by Crowdy. https://www.crowdy.ch",
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  Future<void> _onOpen(LinkableElement link) async {
+    if (await canLaunch(link.url)) {
+      await launch(link.url);
+    } else {
+      throw 'Could not launch $link';
+    }
+  }
 }
+//Text("")
